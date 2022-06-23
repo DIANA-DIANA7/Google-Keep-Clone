c02cbe687f99af1adc6cf3cb1a519d62974d1faf
DIANA-DIANA7 <dianasmejia21@gmail.com>
2022-06-22 17:35:28 -0700

    class in app.js
    Added a class to app.js with methods to to toggle the form open when input is clicked and close the form when clicked out.

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 class App{
     constructor(){
-        console.log("app works!")
+        this.$form=document.querySelector("#form");
+        this.$noteTitle=document.querySelector("#note-title");
+        this.$formButtons=document.querySelector("#form-buttons");
+        this.addEventListenders();
+    }
+
+    addEventListenders(){
+        document.body.addEventListener("click", e =>{
+            this.handleFormClick(e);
+        });
+    }
+
+    handleFormClick(e){
+       const isFormClicked= this.$form.contains(e.target);
+
+       if(isFormClicked){
+        this.openForm();
+       } else{
+        this.closeForm();
+       }
+    }
+    openForm(){
+        this.$form.classList.add("form-open");
+        this.$noteTitle.style.display="block";
+        this.$formButtons.style.display="block";
+    }
+
+    closeForm(){
+        this.$form.classList.remove("form-open");
+        this.$noteTitle.style.display = "none";
+        this.$formButtons.style.display = "none";
     }
 }
 
-new App()
+new App();
