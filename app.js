650bb5b929560c21e2bba647cce55a730d401865
DIANA-DIANA7 <dianasmejia21@gmail.com>
2022-06-22 18:30:47 -0700

    submit button functionality
    addNote method to keep track of note by title, text, color and id then adding
     note to notes array.

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 class App{
     constructor(){
+        this.notes=[];
+        
         this.$form=document.querySelector("#form");
         this.$noteTitle=document.querySelector("#note-title");
+        this.$noteText=document.querySelector("#note-text");
         this.$formButtons=document.querySelector("#form-buttons");
+        
         this.addEventListenders();
     }
 
     addEventListenders(){
         document.body.addEventListener("click", e =>{
             this.handleFormClick(e);
+        });
+
+        this.$form.addEventListener("submit",e=>{
+            e.preventDefault();
+            const title= this.$noteTitle.value;
+            const text= this.$noteText.value;
+            const hasNote= title || text;
+
+            if (hasNote) {
+              this.addNote({title , text})
+            }
         });
     }
 
@@ -32,6 +47,17 @@ class App{
         this.$noteTitle.style.display = "none";
         this.$formButtons.style.display = "none";
     }
+
+    addNote(note){
+        const newNote={
+        title:note.title,
+        text:note.text, 
+        color:"white",
+        id:this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+        };
+        this.notes=[...this.notes,newNote]
+        console.log(this.notes);
+    }  
 }
 
 new App();
