8e25ed0d2f07c30694ae4ba3bcae506d856a5429
DIANA-DIANA7 <dianasmejia21@gmail.com>
2022-06-24 18:58:53 -0700

    close button functionality & select note
    added close button functionality and ability to select note with a modal pop up.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 class App{
     constructor(){
         this.notes=[];
+        this.title="";
+        this.text="";
+        this.id="";
         
         this.$placeholder=document.querySelector("#placeholder");
         this.$form=document.querySelector("#form");
@@ -8,6 +11,10 @@ class App{
         this.$noteTitle=document.querySelector("#note-title");
         this.$noteText=document.querySelector("#note-text");
         this.$formButtons=document.querySelector("#form-buttons");
+        this.$formCloseButton=document.querySelector("#form-close-button");
+        this.$modal=document.querySelector(".modal");
+        this.$modalTitle=document.querySelector(".modal-title");
+        this.$modalText=document.querySelector(".modal-text");
         
         this.addEventListenders();
     }
@@ -15,6 +22,8 @@ class App{
     addEventListenders(){
         document.body.addEventListener("click", e =>{
             this.handleFormClick(e);
+            this.selectNote(e);
+            this.openModal(e);
         });
 
         this.$form.addEventListener("submit",e=>{
@@ -27,14 +36,26 @@ class App{
               this.addNote({title , text})
             }
         });
+        this.$formCloseButton.addEventListener("click", e=>{
+            e.stopPropagation();
+            this.closeForm();
+        })
+
     }
 
     handleFormClick(e){
+        
        const isFormClicked= this.$form.contains(e.target);
+
+       const title = this.$noteTitle.value;
+       const text = this.$noteText.value;
+       const hasNote = title || text;
 
        if(isFormClicked){
         this.openForm();
-       } else{
+       } else if (hasNote){
+        this.addNote({title,text});
+       }else{
         this.closeForm();
        }
     }
@@ -51,11 +72,18 @@ class App{
         this.$noteTitle.value="";
         this.$noteText.value="";
     }
+    openModal(e){
+        if(e.target.closest(".note")){
+            this.$modal.classList.toggle("open-modal")
+            this.$modalTitle.value=this.title;
+            this.$modalText.value=this.text;
+        }
+    }
 
-    addNote(note){
+    addNote({title,text}){
         const newNote={
-        title:note.title,
-        text:note.text, 
+        title,
+        text, 
         color:"white",
         id:this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
         };
@@ -64,13 +92,22 @@ class App{
         this.closeForm();
     }  
 
+    selectNote(e){
+        const $selectedNote=e.target.closest(".note")
+        if(!$selectedNote) return;
+        const [$noteTitle,$noteText]=$selectedNote.children;
+        this.title=$noteTitle.innerText;
+        this.text=$noteText.innerText;
+        this.id=$selectedNote.dataset.id;
+    }
+
     displayNotes(){
         const hasNotes= this.notes.length > 0
         this.$placeholder.style.display=hasNotes ? "none" : "flex";
 
         this.$notes.innerHTML=this.notes.map(
           (note) => `
-            <div style="background: ${note.color}" class="note">
+            <div style="background: ${note.color}" class="note" data-id="${note.id}">
                 <div class="${note.title && 'note-title'}">${note.title}</div>
                 <div class="note-text">${note.text}</div>
                 <div class="toolbar-container">
